d59dbe8ee36589dc75bc1e5227354d0ab49f4d31
seema1611 <seemarajpure16@gmail.com>
2020-07-14 17:04:56 +0700

    ADDED: Feature to add user icon infront of user

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var users=[
     {
         name: "Seema Rajpure",
         email:"seema16@gmail.com",
-        status:"Inactive",
+        status:"Active",
         role:"User",
         lastLogin:"2d ago",
         permisiion:"Valid"
@@ -30,7 +30,7 @@ var users=[
     },{
         name: "Bhakti Arde",
         email:"bhaktiarde29@gmail.com",
-        status:"Inactive",
+        status:"Active",
         role:"Administrator",
         lastLogin:"2d ago",
         permisiion:"Valid"
@@ -55,12 +55,97 @@ var users=[
         role:"User",
         lastLogin:"2d ago",
         permisiion:"Valid"
+    }, {
+        name: "Seema Rajpure",
+        email:"seema16@gmail.com",
+        status:"Active",
+        role:"User",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Ryan Larson",
+        email:"ryanlarson11@gmail.com",
+        status:"Active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Seema Rajpure",
+        email:"seema16@gmail.com",
+        status:"Inactive",
+        role:"User",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Aju Sanas",
+        email:"ajusanas11@gmail.com",
+        status:"Active",
+        role:"User",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Bhakti Arde",
+        email:"bhaktiarde29@gmail.com",
+        status:"Active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Seema Rajpure",
+        email:"seema16@gmail.com",
+        status:"Active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Aishu Kaware",
+        email:"aishu14@gmail.com",
+        status:"Active",
+        role:"User",
+        lastLogin:"3d ago",
+        permisiion:"Valid"
+    },{
+        name: "Aju Sanas",
+        email:"ajusanas11@gmail.com",
+        status:"Active",
+        role:"User",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Aju Sanas",
+        email:"ajusanas11@gmail.com",
+        status:"Active",
+        role:"User",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    }, {
+        name: "Seema Rajpure",
+        email:"seema16@gmail.com",
+        status:"Inactive",
+        role:"User",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Ryan Larson",
+        email:"ryanlarson11@gmail.com",
+        status:"Active",
+        role:"Administrator",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
+    },{
+        name: "Seema Rajpure",
+        email:"seema16@gmail.com",
+        status:"Active",
+        role:"User",
+        lastLogin:"2d ago",
+        permisiion:"Valid"
     }]
     
+   
     var userList=""
     for(var i=0;i<users.length;i++){
         userList+="<div class=\"user-list-item\">\n"+
-        "<div class=\"name list-column\"><i class=\"flaticon-user\"></i>"+users[i].name+"</div>\n"+
+        "<div class=\"name list-column\"><i class=\"fa fa-user-circle\" style=\"color:#555555; margin-right: 10%; font-size:25px;\"></i>"+users[i].name+"</div>\n"+
         "<div class=\"email list-column\">"+users[i].email+"</div>\n";
         console.log(users[i].status);
         if(users[i].status.match("Active")){
@@ -74,6 +159,7 @@ var users=[
         "<button class=\"option list-column\">...</button>\n"+
         "</div>";
     }
+
     window.document.getElementById("user-list-details").innerHTML+=userList;
     var userCount= window.document.getElementsByClassName("user-list-item").length;
     console.log(userCount);
