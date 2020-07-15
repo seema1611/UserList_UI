d083c2b44656674c0a028722e1c007b852309c1b
seema1611 <seemarajpure16@gmail.com>
2020-07-16 00:44:48 +0700

    ADDED: feature to display userlist when click users

--- js/script.js
+++ js/script.js
@@ -141,45 +141,66 @@ var users=[
         permisiion:"Valid"
     }]
     
-   
-    var userList=""
-    for(var i=0;i<users.length;i++){
-        userList+="<div class=\"user-list-item\">\n"+
-        "<div class=\"name list-column\"><i class=\"fa fa-user-circle\" style=\"color:#555555; margin-right: 10%; font-size:25px;\"></i>"+users[i].name+"</div>\n"+
-        "<div class=\"email list-column\">"+users[i].email+"</div>\n";
-        console.log(users[i].status);
-        if(users[i].status.match("Active")){
-            userList+="<div class=\"status list-column\"><div class=\"status-active\">"+users[i].status+"</div> </div>\n";
-        }else{
-            userList+="<div class=\"status list-column\"><div class=\"status-inactive\">"+users[i].status+"</div> </div>\n";
+    function loadUsers(users){
+        var userList=""
+        for(var i=0;i<users.length;i++){
+            userList+="<div class=\"user-list-item\">\n"+
+            "<div class=\"name list-column\"><i class=\"fa fa-user-circle\" style=\"color:#555555; margin-right: 10%; font-size:25px;\"></i>"+users[i].name+"</div>\n"+
+            "<div class=\"email list-column\">"+users[i].email+"</div>\n";
+            console.log(users[i].status);
+            if(users[i].status.match("Active")){
+                userList+="<div class=\"status list-column\"><div class=\"status-active\">"+users[i].status+"</div> </div>\n";
+            }else{
+                userList+="<div class=\"status list-column\"><div class=\"status-inactive\">"+users[i].status+"</div> </div>\n";
+            }
+            userList+="<div class=\"role list-column\">"+users[i].role+"</div>\n"+
+            "<div class=\"last-login list-column\">"+users[i].lastLogin+"</div>\n";
+    
+            console.log(users[i].permisiion);
+            if(users[i].permisiion.match("Valid")){
+                userList+="<div class=\"permission list-column\"><div class=\"permission-valid\">"+users[i].permisiion+"</div> </div>\n";
+            }else{
+                userList+="<div class=\"permission list-column\"><div class=\"permission-invalid\">"+users[i].permisiion+"</div> </div>\n";
+            }
+            userList+="<h7 class=\"option list-column\">...</h7>\n"+
+            "</div>";
         }
-        userList+="<div class=\"role list-column\">"+users[i].role+"</div>\n"+
-        "<div class=\"last-login list-column\">"+users[i].lastLogin+"</div>\n";
-
-        console.log(users[i].permisiion);
-        if(users[i].permisiion.match("Valid")){
-            userList+="<div class=\"permission list-column\"><div class=\"permission-valid\">"+users[i].permisiion+"</div> </div>\n";
-        }else{
-            userList+="<div class=\"permission list-column\"><div class=\"permission-invalid\">"+users[i].permisiion+"</div> </div>\n";
-        }
-        userList+="<button class=\"option list-column\">...</button>\n"+
-        "</div>";
+        window.document.getElementById("user-list-items").innerHTML+=userList;
+        var userCount= window.document.getElementsByClassName("user-list-item").length;
+        window.document.getElementById("user-count").innerHTML = "Users("+userCount+")";
     }
-
-    window.document.getElementById("user-list-details").innerHTML+=userList;
-    var userCount= window.document.getElementsByClassName("user-list-item").length;
-    console.log(userCount);
-    window.document.getElementById("user-count").innerHTML = "Users("+userCount+")";
-    var authenticationSubMenuFlag=true;
+    
+    
+    loadUsers(users);
+    var userToggle=true;
     function showSubMenu(value){
         var subMenuId="sub-menu-"+value;
         var subMenu=document.getElementById(subMenuId);
         console.log(subMenu);
-        if(authenticationSubMenuFlag){
+        if(userToggle){
             subMenu.style.display="block";
-            authenticationSubMenuFlag=false;
+            document.getElementById("authentication").classList.add("menu-button-highlight");
+            userToggle=false;
         }else{
             subMenu.style.display="none";
-            authenticationSubMenuFlag=true;
+            document.getElementById("authentication").classList.remove("menu-button-highlight");
+            userToggle=true;
+        }
+    }
+    
+    
+    var userListToggle=true;
+    function showUsers(){
+        var userList=document.getElementById("user-list");
+        console.log(userListToggle);
+        console.log(userList);
+        if(userListToggle){
+            userList.style.display="block";
+            document.getElementById("users-sub-button").classList.add("menu-button-extra-highlight");
+            userListToggle=false;
+        }else{
+            userList.style.display="none";
+            document.getElementById("users-sub-button").classList.remove("menu-button-extra-highlight");
+            userListToggle=true;
         }
     }
